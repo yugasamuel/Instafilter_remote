47d4063a09fda22f8538b72ff1ecd13595f2b204
Yuga Samuel <yugasamuel2002@gmail.com>
2023-09-16 09:48:46 +0700

    Add user interface

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIView!
+    @IBOutlet var intensity: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
 }
 
